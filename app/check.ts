3a599a22de52daf6ea6f6fc1ef7ff7b8a178bb79
yasufumimyoi <yasufumi.myoi@gmail.com>
2025-02-20 21:00:45 +0900

    feat:エラー確認

--- app/check.ts
+++ app/check.ts
@@ -6,3 +6,16 @@ console.log(test) // ← 未使用の変数（ESLint ルール違反）
 const foo = { bar: 'baz' } 
 console.log( foo.bar );
 console.log( foo.bar );
+console.log( foo.bar );
+
+// example.ts
+
+// 型エラーを発生させるために、number型にstringを代入
+let numberVariable: number = "this is a string";  // 型 'string' を型 'number' に割り当てることはできません
+
+// 関数に間違った引数を渡す
+function greet(name: string): void {
+  console.log(`Hello, ${name}`);
+}
+
+greet(123);  // 引数 '123' は型 'string' に割り当てることができません
