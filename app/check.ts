aff212f476147990310a7f4205fd2b44f2350ae7
yasufumimyoi <yasufumi.myoi@gmail.com>
2025-02-21 20:21:46 +0900

    test

--- app/check.ts
+++ app/check.ts
@@ -10,7 +10,7 @@ console.log( foo.bar );
 
 // example.ts
 consoleaaa
-
+a
 // 型エラーを発生させるために、number型にstringを代入
 let numberVariable: number = "this is a string";  // 型 'string' を型 'number' に割り当てることはできません
 
@@ -20,3 +20,21 @@ function greet(name: string): void {
 }
 
 greet(123);  // 引数 '123' は型 'string' に割り当てることができません
+
+const message = "Hello, world!";
+console.log(message);
+
+function myFunction() {
+  let unusedVariable; // 未使用の変数
+  console.log("Function called");
+}
+
+myFunction();
+
+// index.js
+const message = "Hello, world!";
+console.log(message);
+
+function myFunction() {
+  console.log("Function called");
+}
